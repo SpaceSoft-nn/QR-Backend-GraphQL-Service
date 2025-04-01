d38b9dd9a71d28f4d174af773dff80a20986f87d
Xpyst <qjq3@mail.ru>
2025-04-01 16:17:54 +0300

    добавил в api endpoint на controller graphql

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Nuwave\Lighthouse\Support\Http\Controllers\GraphQLController;
+use Nuwave\Lighthouse\Http\GraphQLController;
 
-Route::post('/graphql', [GraphQLController::class, 'query']);
+Route::post('/graphql', GraphQLController::class);
